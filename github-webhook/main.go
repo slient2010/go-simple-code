adb516929a03535b5f4a2c11584a61060308fddc
slient2010 <slient2010@163.com>
2018-09-20 15:24:17 +0800

    ✨ Feat: add github webhook
    add github webhook

---
+++ github-webhook/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/go-github/github"
+	"log"
+	"net/http"
+)
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	payload, err := github.ValidatePayload(r, []byte("my-secret-key"))
+	if err != nil {
+		log.Printf("error validating request body: err=%s\n", err)
+		return
+	}
+	defer r.Body.Close()
+
+	event, err := github.ParseWebHook(github.WebHookType(r), payload)
+	if err != nil {
+		log.Printf("could not parse webhook: err=%s\n", err)
+		return
+	}
+
+	switch e := event.(type) {
+	case *github.PushEvent:
+		// this is a commit push, do something with it
+	case *github.PullRequestEvent:
+		// this is a pull request, do something with it
+	case *github.WatchEvent:
+		// https://developer.github.com/v3/activity/events/types/#watchevent
+		// someone starred our repository
+		if e.Action != nil && *e.Action == "starred" {
+			fmt.Printf("%s starred repository %s\n",
+				*e.Sender.Login, *e.Repo.FullName)
+		}
+	default:
+		log.Printf("unknown event type %s\n", github.WebHookType(r))
+		return
+	}
+}
+
+func main() {
+	log.Println("server started")
+	http.HandleFunc("/webhook", handleWebhook)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
